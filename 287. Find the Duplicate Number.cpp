1ca245cb46ad58da53404365775e7035f3c50722
sams3344 <68379863+sams3344@users.noreply.github.com>
2020-07-16 17:11:22 +0530

    Create 287. Find the Duplicate Number.cpp

---
+++ 287. Find the Duplicate Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int index;
+        for(int i=0; i<nums.size(); i++){
+            index = abs(nums[i]) - 1;
+            nums[index] *= -1;
+            if(nums[index] > 0){
+                return abs(nums[i]);
+            }
+        }
+        return -1;
+    }
+};

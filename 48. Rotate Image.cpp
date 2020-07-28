8038a1af360e7db458205800e3a14bfe866cbd54
Sam <68379863+sams3344@users.noreply.github.com>
2020-07-28 12:28:15 +0530

    Create 48. Rotate Image.cpp

---
+++ 48. Rotate Image.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    void rotate(vector<vector<int>>& m) {
+      
+        int n = m.size();
+        
+        for(int i =0; i< n ;i++){
+            for(int j=i;j< n ;j++){
+                swap(m[i][j],m[j][i]);
+            }
+        }
+        
+        for(int i =0; i< n ;i++){
+            for(int j=0;j< n ;j++){
+                cout << m[i][j];
+            }
+            cout << "\n";
+        }
+        
+     for(int i =0;i< n ; i++){
+         reverse(m[i].begin(), m[i].end());
+     }
+                                
+
+    }
+};
